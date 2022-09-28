7a6e3058397aba5b5b43133783fdf24391859397
Cho Lommerse <cho.lommerse@ziggo.nl>
2022-09-28 11:14:15 +0200

    aanpassingen

--- dashboard.php
+++ dashboard.php
@@ -3,9 +3,24 @@
 session_start();
 
 if (!$_SESSION["isLoggedIn"]) {
-    echo "Deze gebruiker mag hier niet komen"
+    echo "Deze gebruiker mag hier niet komen";
 }
 
 echo "Je bent op de dashboard";
 ?>
-<a href="logout.php">logout</a>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <a href="logout.php">logout</a>
+</body>
+
+</html>
