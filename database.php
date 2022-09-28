30bc49b69a821479e8092f09d89698921b145608
Cho Lommerse <cho.lommerse@ziggo.nl>
2022-09-28 08:55:24 +0200

    deze aangepast

--- database.php
+++ database.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 // Database configuratie
 $host  = "localhost";
@@ -8,14 +7,3 @@ $dbname = "snackbartest";
 
 // Maak een  database connectie
 $conn = mysqli_connect($host, $dbuser, $dbpass, $dbname);
-=======
-<?php
-// Database configuratie
-$host  = "localhost";
-$dbuser = "root";
-$dbpass = "";
-$dbname = "snackbartest";
-
-// Maak een  database connectie
-$conn = mysqli_connect($host, $dbuser, $dbpass, $dbname);
->>>>>>> 21e53a5e6aa20e4084365e62a2f2caec7f96b666
