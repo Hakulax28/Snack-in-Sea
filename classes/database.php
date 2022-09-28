112e91d2c0d1bb5e75a5984cfde11074e8913a9f
Cho Lommerse <cho.lommerse@ziggo.nl>
2022-09-28 11:13:06 +0200

    aanpassingen

--- classes/database.php
+++ classes/database.php
@@ -1,31 +1,28 @@
 <?php
+
 // Database configuratie
-//$host  = "localhost";
-//$dbuser = "root";
-//$dbpass = "";
-//$dbname = "snackbartest";
+
 
 // Maak een  database connectie
-//$conn = mysqli_connect($host, $dbuser, $dbpass, $dbname);
+
 
 class Database
 {
-   public $host  = "localhost";
-   public $dbuser = "root";
-   public $dbpass = "";
-   public $dbname = "snackbartest";
 
-   private $conn;
+    private $host  = "localhost";
+    private $dbuser = "root";
+    private $dbpass = "";
+    private $dbname = "fake-app";
 
-   public function __construct()
-   {
-      $this->conn = mysqli_connect($this->host, $this->dbuser, $this->dbpass, $this->dbname);
-   }
-   public function getConnection()
-   {
-      return $this->conn;
-   }
+    private $conn;
+
+    public function __construct()
+    {
+        $this->conn =  mysqli_connect($this->host, $this->dbuser, $this->dbpass, $this->dbname);
+    }
+
+    public function getConnection()
+    {
+        return $this->conn;
+    }
 }
-
-$databaseConnection = new Database();
-$conn = $databaseConnection->getConnection();
