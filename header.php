3ce360d882eccd16c9ddadcd7dfffb35fbd5e42e
Cho Lommerse <cho.lommerse@ziggo.nl>
2022-09-28 10:55:08 +0200

    een bestand toegevoegt

---
+++ header.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="signin.css">
+</head>
+
+<body style="background-color: #656599; margin: auto;">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark border-bottom">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Navigatie</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Hoofdpagina</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="gebruiker-overzicht.php">Overzicht gebruikers</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="melding-overzicht.php">Overzicht melding</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="categorie-overzicht.php">Overzicht categories</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="container mt-3">
