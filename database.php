21e53a5e6aa20e4084365e62a2f2caec7f96b666
Hakulax28 <83372318+Hakulax28@users.noreply.github.com>
2022-09-28 08:52:57 +0200

    dingen veranderd

---
+++ database.php
@@ -0,0 +1,9 @@
+<?php
+// Database configuratie
+$host  = "localhost";
+$dbuser = "root";
+$dbpass = "";
+$dbname = "snackbartest";
+
+// Maak een  database connectie
+$conn = mysqli_connect($host, $dbuser, $dbpass, $dbname);
