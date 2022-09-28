34bb9a33caa9dce98ff06d4ee340584932f69f3e
Cho Lommerse <cho.lommerse@ziggo.nl>
2022-09-28 10:55:44 +0200

    een bestand toegevoegt

---
+++ footer.php
@@ -0,0 +1,16 @@
+</div>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+</body>
+<!-- Footer -->
+<footer class="bg-dark text-center text-white">
+   <!-- Grid container -->
+   <div class="container p-4">
+      <!-- Copyright -->
+      <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">© 2019 - 2022: Cho Lommerse</div>
+      <!-- Copyright -->
+   </div>
+</footer>
+<!-- Footer -->
+
+</html>
