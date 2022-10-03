2435cee6aaecb193ee159b64adf3f7f259e1a10b
Cho Lommerse <cho.lommerse@ziggo.nl>
2022-10-03 08:45:42 +0200

    de dingen van index.html toegevoegd

--- dashboard.php
+++ dashboard.php
@@ -16,11 +16,23 @@ echo "Je bent op de dashboard";
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <title>Snack'in Sea</title>
 </head>
 
 <body>
-    <a href="logout.php">logout</a>
+    <header>
+        <p>Test</p>
+    </header>
+    <div class="main">
+        Test
+        <div>
+
+        </div>
+    </div>
+    <footer>
+
+    </footer>
 </body>
 
 </html>
