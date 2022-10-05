783b510b410b1e29939da8232ef0639a6bb3e2ec
Cho Lommerse <cho.lommerse@ziggo.nl>
2022-10-05 11:27:42 +0200

    dingen aangepast en bestand toegevoegd

--- bestel-update.php
+++ bestel-update.php
@@ -2,48 +2,57 @@
 
 session_start();
 
-require 'database.php';
+require 'classes/database.php';
 
 $id = $_GET["id"]; //17
 
-$sql = "SELECT * FROM message WHERE id = $id LIMIT 1";
+$sql = "SELECT * FROM bestel WHERE id = $id LIMIT 1";
 
-if ($result = mysqli_query($conn, $sql)) {
+if ($result = mysqli_query((new Database())->getConnection(), $sql)) {
 
     $user = mysqli_fetch_assoc($result);
 
     if (is_null($user)) {
-        header("location: melding-overzicht.php");
+        header("location: bestel-overzicht.php");
     }
 }
 
 ?>
-<?php include 'header.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
 
-<form action="update-melding-verwerk.php" method="post">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
 
-    <input type="hidden" name="id" value="<?php echo $user["id"] ?>">
+<body>
 
-    <div class=" form-group">
-        <label for="bericht">Bericht</label>
-        <input type="text" name="bericht" id="bericht" class="form-control" value="<?php echo $user["bericht"] ?>">
-    </div><br>
-    <div class=" form-group">
-        <label for="status">Status</label>
-        <input type="text" name="status" id="status" class="form-control" value="<?php echo $user["status"] ?>">
-    </div><br>
-    <div class="form-group">
-        <label for="datum">Datum</label>
-        <input type="date" name="datum" id="datum" class="form-control" value="<?php echo $user["datum"] ?>">
-    </div><br>
-    <div class="form-group">
-        <label for="opmerking">Opmerking van personeel</label>
-        <input type="text" name="opmerking" id="opmerking" class="form-control" value="<?php echo $user["opmerking"] ?>">
-    </div><br>
-    <div class=" form-group">
-        <button type="submit" class="shadow-sm btn btn-info" name="submit">Update gebruiker!</button>
-        <a href="melding-overzicht.php" class="shadow-sm btn btn-danger">Annuleer</a>
-    </div><br>
+    <form action="update-bestel-verwerk.php" method="post">
 
-</form>
-<?php include 'footer.php'; ?>
+        <input type="hidden" name="id" value="<?php echo $user["id"] ?>">
+
+        <div class=" form-group">
+            <label for="product">De producten</label>
+            <input type="text" name="bericht" id="bericht" class="form-control" value="<?php echo $user["bericht"] ?>">
+        </div><br>
+        <div class=" form-group">
+            <label for="status">Status</label>
+            <input type="text" name="status" id="status" class="form-control" value="<?php echo $user["status"] ?>">
+        </div><br>
+        <div class="form-group">
+            <label for="tijd">Tijd</label>
+            <input type="date" name="datum" id="datum" class="form-control" value="<?php echo $user["datum"] ?>">
+        </div><br>
+        <div class=" form-group">
+            <button type="submit" class="shadow-sm btn btn-info" name="submit">Update gebruiker!</button>
+            <a href="bestel-overzicht.php" class="shadow-sm btn btn-danger">Annuleer</a>
+        </div><br>
+
+    </form>
+
+</body>
+
+</html>
